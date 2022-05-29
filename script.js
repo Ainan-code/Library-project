ebcd753d057bedff6869119035792f64e5211113
Ainan-code <ainanhassan123@gmail.com>
2022-05-29 12:35:18 +0300

    made the loop work for the array n display

--- script.js
+++ script.js
@@ -12,20 +12,27 @@ function addBooktoLibrary () {
   let title = document.getElementById('title');
   let author = document.getElementById('author');
   let page = document.getElementById('page'); 
-  myLibrary.push(title.value);
-  myLibrary.push(author.value);
-  myLibrary.push(page.value)
+ 
+  const newBook = new Book(title.value, author.value, page.value);
+  myLibrary.push(newBook)
+
 
 }
 
+const book2 = new Book('hokins', 'Ainan', 50);
+const book3 = new Book('hokins', 'Abdi', 50);
+
+myLibrary.push(book2, book3);
+
+
 function displayBook() {
   // looping trough the array
-  myLibrary.forEach((item)=>{
-    let display = document.getElementById('display');
-    let li = document.createElement("div");
-    li.innerText = item;
-    display.append(li);
-  })
+  let display = document.getElementById('display')
+  for (let i=0; i < myLibrary.length; i++) {
+   let div = document.createElement('div')
+   div.innerText = `${myLibrary[i].title} +  ${myLibrary[i].author} + ${myLibrary[i].pages}`
+   display.appendChild(div)
+  }
 }
 
 displayBook();
